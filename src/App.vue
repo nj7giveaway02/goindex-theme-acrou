1d0e1a5c63810806e39e6eb7eaea9aa44dd0bf14
nj7giveaway02 <72903987+nj7giveaway02@users.noreply.github.com>
2020-12-23 17:32:15 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
   },
   data: function() {
     return {
-      github: "https://github.com/Aicirou/goindex-theme-acrou",
+      github: "https://github.com/nj7giveaway02/goindex-theme-acrou",
     };
   },
   watch: {
